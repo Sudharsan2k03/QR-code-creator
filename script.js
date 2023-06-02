dbfa4dda56b1dd084ec17076e150490461e8e2f2
Sudharsan R <135221837+Sudharsan2k03@users.noreply.github.com>
2023-06-02 19:04:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let qrImgDiv=document.getElementById('qrImg')
+let button=document.getElementById('btn')
+let qrTextDiv=document.getElementById('qrText')
+
+function generateQR(){
+    qrImgDiv.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrTextDiv.value
+}
+button.onclick=()=>{
+    generateQR()
+}
